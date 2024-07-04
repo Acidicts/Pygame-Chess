d59ce03333fa49a187671a0552f842a79b4084a6
Acidicts <111660544+Acidicts@users.noreply.github.com>
2024-07-04 17:02:56 +0100

    Commit 3

--- classes.py
+++ classes.py
@@ -1,7 +1,5 @@
 import pygame
-
 from grid import get_grid_square
-
 
 class Piece:
     def __init__(self, x, y, image):
@@ -19,14 +17,12 @@ class Piece:
             self.show_moves(screen)
 
     def move(self, pos):
-        if self.selected:
-            grid_pos = get_grid_square(pos)
-            valid_moves = self.white_moves if self.direction == 1 else self.black_moves
-            for move in valid_moves:
-                if grid_pos == move:
-                    self.x, self.y = move[0] * 80, move[1] * 80
-                    self.rect.topleft = (self.x, self.y)
-                    break  # Exit the loop once the move is made
+        grid_pos = get_grid_square(pos)
+        valid_moves = self.white_moves if self.direction == 1 else self.black_moves
+        if grid_pos in valid_moves:
+            self.x, self.y = grid_pos[0] * 80, grid_pos[1] * 80
+            self.rect.topleft = (self.x, self.y)
+            self.first_move = False
 
     def show_moves(self, screen):
         pass
@@ -34,32 +30,42 @@ class Piece:
     def clicked(self, pos):
         return self.rect.collidepoint(pos)
 
-
 class Void(Piece):
     def __init__(self, x, y):
-        super().__init__(x, y, None)
+        super().__init__(x, y, None)  # No image for Void spaces
+        self.direction = 0  # Void spaces do not have a direction
 
     def draw(self, screen):
-        pass
+        pass  # Void spaces do not need to be drawn
+
+    def show_moves(self, screen):
+        pass  # Void spaces do not have moves
 
     def move(self, pos):
-        pass
+        pass  # Void spaces cannot move
 
+    def clicked(self, pos):
+        return False  # Clicks on Void spaces should not register as selecting a piece
 
 class Pawn(Piece):
     def __init__(self, x, y, image):
         super().__init__(x, y, image)
         self.first_move = True
-        self.direction = 0
-        self.black_moves = [self.rect.center[0], self.y + 40 + 80, self.rect.center[0], self.y + 40 + 160]
-        self.white_moves = [self.rect.center[0], self.y + 40 - 80, self.rect.center[0], self.y + 40 - 160]
+        self.direction = 0  # Set to 1 for white, -1 for black in the game setup
 
     def show_moves(self, screen):
-        if self.direction == -1:
-            pygame.draw.circle(screen, (0, 255, 0), self.moves[0], 10)
+        self.white_moves = []
+        self.black_moves = []
+        grid_x, grid_y = get_grid_square((self.x, self.y))
+
+        if self.direction == 1:  # White pawn
+            self.white_moves.append((grid_x, grid_y - 1))
             if self.first_move:
-                pygame.draw.circle(screen, (0, 255, 0), (self.rect.center[0], self.y + 40 + 160), 10)
-        elif self.direction == 1:
-            pygame.draw.circle(screen, (0, 255, 0), (self.rect.center[0], self.y + 40 - 80), 10)
+                self.white_moves.append((grid_x, grid_y - 2))
+        elif self.direction == -1:  # Black pawn
+            self.black_moves.append((grid_x, grid_y + 1))
             if self.first_move:
-                pygame.draw.circle(screen, (0, 255, 0), (self.rect.center[0], self.y + 40 - 160), 10)
+                self.black_moves.append((grid_x, grid_y + 2))
+
+        for move in self.white_moves + self.black_moves:
+            pygame.draw.circle(screen, (0, 255, 0), ((move[0] * 80) + 40, (move[1] * 80) + 40), 10)
